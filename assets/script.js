3f6f895edab026608be4027259c1efc26571d738
hlays <lays.hidani@gmail.com>
2019-02-20 21:48:59 -0300

    exercicio guiado completo

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,37 @@
+var dropMenu = document.getElementById('drop-menu');
 
+dropMenu.addEventListener('change', carregaProgramadoras);
 
+window.onload = carregaMenu();
 
-// function carregaMenu(){
+function carregaMenu(){
+  var nome = document.createElement('option');
+  nome.innerHTML = 'Selecione a sede';
+  nome.value = 'none';
 
-// };
+  dropMenu.appendChild(nome);
 
-function carregaProgramadoras(){
+  for (sede in data) {
+    var itemMenu = document.createElement('option');
+
+    itemMenu.value = sede;
+    itemMenu.innerHTML = sede;
+    dropMenu.appendChild(itemMenu);
+  }
+};
+
+function carregaProgramadoras() {
+  var sede = dropMenu.value;
+  var listaProgramadoras = document.getElementById('exibe-programadoras');
+
+  listaProgramadoras.innerHTML = '';
+
+  for (turma in data[sede]) {
+    for (i in data[sede][turma]['students']) {
+      var img = document.createElement('img');
+      img.src = data[sede][turma]['students'][i]['photo'];
+      listaProgramadoras.appendChild(img);
+    }
+  }
 
 };
